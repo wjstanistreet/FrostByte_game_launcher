48d86065e969b7d5588c4f0b7a8a1f25ed7b06cf
Fatimah Patel <fatimahpatel10@gmail.com>
2022-12-12 17:09:38 +0000

    Created second, third and fourth accounts.

--- src/main/java/com/example/frostbyte_game_launcher/components/DataLoader.java
+++ src/main/java/com/example/frostbyte_game_launcher/components/DataLoader.java
@@ -23,8 +23,14 @@ public class DataLoader implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
 
         //Account Creation
-        Account accountOne = new Account("Will Stanistreet", "12345", "01/01/1942", "will@will.com" );
+        Account accountOne = new Account("Will Stanistreet", "12345", "01/01/1992", "will@will.com" );
         accountRepository.save(accountOne);
+        Account accountTwo = new Account("Shiyi Zhu", "88886", "08/08/1988", "shiyi@shiyi.com");
+        accountRepository.save(accountTwo);
+        Account accountThree = new Account("Eesaa Sheikh", "63729", "05/01/1996", "eesaa@eesaa.com");
+        accountRepository.save(accountThree);
+        Account accountFour = new Account("Fatimah Patel", "11111", "03/03/1993", "fatimah@fatimah.com");
+        accountRepository.save(accountFour);
 
     }
 
