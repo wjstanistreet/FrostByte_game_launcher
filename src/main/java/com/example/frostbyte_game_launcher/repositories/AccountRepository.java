36426d6c94cd3d9f6716d71fc48e12ad487c0c3d
Fatimah Patel <fatimahpatel10@gmail.com>
2022-12-12 16:41:00 +0000

    Added Repository annotations and extends JpaRepository to Game and Account repositories.

--- src/main/java/com/example/frostbyte_game_launcher/repositories/AccountRepository.java
+++ src/main/java/com/example/frostbyte_game_launcher/repositories/AccountRepository.java
@@ -1,4 +1,10 @@
 package com.example.frostbyte_game_launcher.repositories;
 
-public interface AccountRepository {
+import com.example.frostbyte_game_launcher.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
 }
