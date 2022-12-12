959cb69fd3e70112426d16a40c5917c856047cf3
Shiyi Zhu <puckyyshiyi1024@gmail.com>
2022-12-12 16:01:37 +0000

    Created Account model

---
+++ src/main/java/com/example/frostbyte_game_launcher/models/Account.java
@@ -0,0 +1,59 @@
+package com.example.frostbyte_game_launcher.models;
+
+public class Account {
+
+    private long id;
+    private String name;
+    private String password;
+    private String dateOfBirth;
+    private String email;
+
+    public Account (){};
+
+    public Account(String name, String password, String dateOfBirth, String email){
+        this.name = name;
+        this.password = password;
+        this.dateOfBirth = dateOfBirth;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
