f1461d55a94c4358a4b1e5dce12b607782cc2856
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-12-12 11:26:36 +0000

    initial commit

---
+++ src/main/java/com/example/frostbyte_game_launcher/FrostbyteGameLauncherApplication.java
@@ -0,0 +1,13 @@
+package com.example.frostbyte_game_launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrostbyteGameLauncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrostbyteGameLauncherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/frostbyte_game_launcher/FrostbyteGameLauncherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frostbyte_game_launcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrostbyteGameLauncherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
