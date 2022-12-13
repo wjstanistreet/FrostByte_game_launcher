64356b08622acbe6020ec8e67ea4e9754d736f59
Shiyi Zhu <puckyyshiyi1024@gmail.com>
2022-12-13 11:52:31 +0000

    added function to create new account

--- src/main/java/com/example/frostbyte_game_launcher/controllers/AccountController.java
+++ src/main/java/com/example/frostbyte_game_launcher/controllers/AccountController.java
@@ -1,13 +1,12 @@
 package com.example.frostbyte_game_launcher.controllers;
 
 import com.example.frostbyte_game_launcher.models.Account;
+import com.example.frostbyte_game_launcher.repositories.AccountRepository;
 import com.example.frostbyte_game_launcher.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +16,18 @@ public class AccountController {
 
     @Autowired
     AccountService accountService;
+    @Autowired
+    private AccountRepository accountRepository;
 
     @GetMapping
     public ResponseEntity<List<Account>> getAllAccounts(){
         List<Account> allAccounts = accountService.getAllAccounts();
         return new ResponseEntity<>(allAccounts, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Account> addNewAccount(@RequestBody Account account){
+        accountRepository.save(account);
+        return new ResponseEntity<>(account, HttpStatus.CREATED);
     }
 }
