57c34821afa06c1d1beb9697184027ff15304751
Shiyi Zhu <puckyyshiyi1024@gmail.com>
2022-12-13 11:04:20 +0000

    Added function to get all games

--- src/main/java/com/example/frostbyte_game_launcher/controllers/GameController.java
+++ src/main/java/com/example/frostbyte_game_launcher/controllers/GameController.java
@@ -1,4 +1,28 @@
 package com.example.frostbyte_game_launcher.controllers;
 
+import com.example.frostbyte_game_launcher.models.Account;
+import com.example.frostbyte_game_launcher.models.Game;
+import com.example.frostbyte_game_launcher.services.AccountService;
+import com.example.frostbyte_game_launcher.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/games")
 public class GameController {
+
+    @Autowired
+    GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<List<Game>> getAllGames(){
+        List<Game> allGames = gameService.getAllGames();
+        return new ResponseEntity<>(allGames, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/frostbyte_game_launcher/services/GameService.java
+++ src/main/java/com/example/frostbyte_game_launcher/services/GameService.java
@@ -1,9 +1,21 @@
 package com.example.frostbyte_game_launcher.services;
 
+import com.example.frostbyte_game_launcher.models.Account;
+import com.example.frostbyte_game_launcher.models.Game;
+import com.example.frostbyte_game_launcher.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GameService {
 
+    @Autowired
+    GameRepository gameRepository;
+
+    public List<Game> getAllGames(){
+        return gameRepository.findAll();
+    }
 
 }
